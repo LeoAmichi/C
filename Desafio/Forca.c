1302fc1e0fc1f806233c495efba2102e3468d24e
Leonardo Amichi <leoamichi1801@gmail.com>
2024-09-18 20:50:56 -0300

    .

--- Desafio/Forca.c
+++ Desafio/Forca.c
@@ -112,7 +112,7 @@ int main()
     for (int i = 0; i < 5; i++)
         strcpy(string[i], sortear(random[i]));
 
-    //inicializando todos os caracteres da string progresso com - para ser oculto
+    // inicializando todos os caracteres da string progresso com - para ser oculto
     for (int i = 0; i < 5; i++)
     {
         int j = 0;
@@ -168,20 +168,23 @@ int main()
             pontosRodada += pontos(string[i], chute);
 
         chances++;
-        
+
         letras_usadas[j] = chute;
         letras_usadas[j + 1] = '\0';
 
         printf("\nLetras usadas: ");
-        for (int i = 0; letras_usadas[i] != '\0'; i++){
+        for (int i = 0; letras_usadas[i] != '\0'; i++)
+        {
             printf("%c ", letras_usadas[i]);
-            for (int j = 0; j <= i; j++) { //Verificação de Repetição de letras já usadas
-                if (i > 0 && letras_usadas[i - j] == chute) {
-                pontosRodada = 0; 
-                break;
+            for (int j = 0; j <= i; j++)
+            { // Verificação de Repetição de letras já usadas
+                if (i > 0 && letras_usadas[i - j] == chute)
+                {
+                    pontosRodada = 0;
+                    break;
+                }
             }
         }
-    }
         if (pontosRodada > 0)
             chances--;
 
@@ -198,4 +201,3 @@ int main()
     }
     return 0;
 }
-
